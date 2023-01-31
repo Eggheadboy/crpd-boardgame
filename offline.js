f754a4f9da734a5370573ba58afd7d345fd2f215
Eggheadboy <38121899+Eggheadboy@users.noreply.github.com>
2023-01-31 10:11:01 +0800

    1.2.3
    update

---
+++ offline.js
@@ -0,0 +1,31 @@
+﻿{
+	"version": 1675130986,
+	"fileList": [
+		"data.js",
+		"c2runtime.js",
+		"jquery-3.4.1.min.js",
+		"offlineClient.js",
+		"images/遊戲封面-sheet0.png",
+		"images/進入遊戲-sheet0.png",
+		"images/背景-sheet0.png",
+		"images/肢體障礙-sheet0.png",
+		"images/普通人-sheet0.png",
+		"images/視覺障礙-sheet0.png",
+		"images/聽覺障礙-sheet0.png",
+		"images/遊戲背景-sheet0.png",
+		"images/手牌1-sheet0.png",
+		"images/手牌4-sheet0.png",
+		"images/開始回合-sheet0.png",
+		"images/抽一張卡1-sheet0.png",
+		"images/結束回合-sheet0.png",
+		"images/使用手牌1-sheet0.png",
+		"images/捨棄手牌1-sheet0.png",
+		"images/走一步-sheet0.png",
+		"icon-16.png",
+		"icon-32.png",
+		"icon-114.png",
+		"icon-128.png",
+		"icon-256.png",
+		"loading-logo.png"
+	]
+}
